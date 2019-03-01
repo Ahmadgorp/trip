ce782fc58470cc2363c9e50bd42f44ecae0c043e
ahmad <ahmadnomii92@gmail.com>
2019-03-01 12:17:03 +0300

    make login/register/add new page for future used+ firebase

---
+++ src/app/firebase.ts
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyBI5u7zAIeK43dm8eTYNGCRMSHXNhjCxQ4",
+    authDomain: "trip1-9dcee.firebaseapp.com",
+    databaseURL: "https://trip1-9dcee.firebaseio.com",
+    projectId: "trip1-9dcee",
+    storageBucket: "trip1-9dcee.appspot.com",
+    messagingSenderId: "634805653014"
+  }
+
+  export default config
